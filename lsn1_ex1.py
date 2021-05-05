be2b3a01cf40b0608e9f3d11df9ec60bd0c8f581
T-Leo <dima2310asd@gmail.com>
2021-05-06 00:18:33 +0500

    lesson #1 _1

---
+++ lsn1_ex1.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2.3
+c = 'uuu'
+d = 'ooo'
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+a = int(input('enter the integer- '))
+b = float(input('enter the fractional number- '))
+c = str(input('enter the first sentence- '))
+d = str(input('enter the second sentence- '))
+
+print(a)
+print(b)
+print(c)
+print(d)
