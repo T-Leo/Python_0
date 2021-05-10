11d72e7dd1fab69e48a505fe5c2f7110193b71c0
T-Leo <dima2310asd@gmail.com>
2021-05-10 22:53:55 +0500

    второй урок

---
+++ 02/lsn02_ex03_b.py
@@ -0,0 +1,20 @@
+monthNum = int(input('enter the number of the month- '))
+while monthNum > 12 or monthNum < 1:
+    monthNum = int(input('enter the number of the month- '))
+
+# print(monthNum)
+
+monthDict = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+print('the month is- ', (monthDict.get(monthNum)))
+
+if monthNum < 3 or monthNum == 12:
+    print("it's a winter")
+
+elif monthNum > 2 and monthNum < 6:
+    print("it's a spring")
+
+elif monthNum > 5 and monthNum < 9:
+    print("it's a summer")
+
+else:
+    print("it's an autumn")
---
+++ 02/lsn02_ex05.py
@@ -0,0 +1,11 @@
+raiting = [7, 5, 3, 3, 2]
+
+new_value = int(input('enter the new value- '))
+while new_value < 1:
+    new_value = int(input('enter the new value- '))
+
+new_value = float
+
+raiting.append(new_value)
+raiting.sort(reverse=True)
+print(raiting)
